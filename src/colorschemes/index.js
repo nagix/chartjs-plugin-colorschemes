659c3cc15b0525d3a421fd609c5077307ec2bf22
Akihiko Kusanagi <nagi@nagi-p.com>
2019-01-24 00:03:13 +0700

    Add MS Office color schemes

--- src/colorschemes/index.js
+++ src/colorschemes/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import brewer from './colorschemes.brewer';
+import office from './colorschemes.office';
 import tableau from './colorschemes.tableau';
 
 export default {
 	brewer: brewer,
+	office: office,
 	tableau: tableau
 };
