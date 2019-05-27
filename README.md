20c60e596341774e83a5ecfc899d8e707c521742
Akihiko Kusanagi <nagi@nagi-p.com>
2019-05-27 15:46:44 +0800

    Add override option

--- README.md
+++ README.md
@@ -77,6 +77,7 @@ All available options are listed below.
 | `fillAlpha` | `Number` | `0.5` | The transparency value for the line fill color. Must be a number between `0.0` (fully transparent) and `1.0` (no transparency).
 | `scheme` | `String` | `'brewer.Paired12'` | Color scheme name that is any of [Color Chart](https://nagix.github.io/chartjs-plugin-colorschemes/colorchart.html).
 | `reverse` | `Boolean` | `false` | If set to `true`, the order of the colors in the selected  scheme is reversed.
+| `override` | `Boolean` | `false` | If set to `true`, the specified color scheme will override the existing color options. If `false`, it is only applied when no color setting exists.
 | `custom` | `Function` | `undefined` | A function that takes a copy of the color string array for `scheme` in order to extend the predefined scheme colors. [more...](#custom-function).
 
 ### `custom`-Function
--- src/plugins/plugin.colorschemes.js
+++ src/plugins/plugin.colorschemes.js
@@ -8,10 +8,13 @@ var helpers = Chart.helpers;
 // Element models are always reset when hovering in Chart.js 2.7.2 or earlier
 var hoverReset = Chart.DatasetController.prototype.removeHoverStyle.length === 2;
 
+var EXPANDO_KEY = '$colorschemes';
+
 Chart.defaults.global.plugins.colorschemes = {
 	scheme: 'brewer.Paired12',
 	fillAlpha: 0.5,
-	reverse: false
+	reverse: false,
+	override: false
 };
 
 export default {
@@ -20,6 +23,9 @@ export default {
 	beforeUpdate: function(chart, options) {
 		var s = options.scheme.split('.');
 		var category = colorschemes[s[0]];
+		var fillAlpha = options.fillAlpha;
+		var reverse = options.reverse;
+		var override = options.override;
 		var scheme, schemeClone, length, colorIndex, color;
 
 		if (category) {
@@ -43,49 +49,49 @@ export default {
 				// Set scheme colors
 				chart.config.data.datasets.forEach(function(dataset, datasetIndex) {
 					colorIndex = datasetIndex % length;
-					color = schemeClone[options.reverse ? length - colorIndex - 1 : colorIndex];
+					color = schemeClone[reverse ? length - colorIndex - 1 : colorIndex];
 
 					// Object to store which color option is set
-					dataset.colorschemes = {};
+					dataset[EXPANDO_KEY] = {};
 
 					switch (dataset.type || chart.config.type) {
 					// For line, radar and scatter chart, borderColor and backgroundColor (50% transparent) are set
 					case 'line':
 					case 'radar':
 					case 'scatter':
-						if (typeof dataset.backgroundColor === 'undefined') {
-							dataset.backgroundColor = helpers.color(color).alpha(options.fillAlpha).rgbString();
-							dataset.colorschemes.backgroundColor = true;
+						if (typeof dataset.backgroundColor === 'undefined' || override) {
+							dataset[EXPANDO_KEY].backgroundColor = dataset.backgroundColor;
+							dataset.backgroundColor = helpers.color(color).alpha(fillAlpha).rgbString();
 						}
-						if (typeof dataset.borderColor === 'undefined') {
+						if (typeof dataset.borderColor === 'undefined' || override) {
+							dataset[EXPANDO_KEY].borderColor = dataset.borderColor;
 							dataset.borderColor = color;
-							dataset.colorschemes.borderColor = true;
 						}
-						if (typeof dataset.pointBackgroundColor === 'undefined') {
-							dataset.pointBackgroundColor = helpers.color(color).alpha(options.fillAlpha).rgbString();
-							dataset.colorschemes.pointBackgroundColor = true;
+						if (typeof dataset.pointBackgroundColor === 'undefined' || override) {
+							dataset[EXPANDO_KEY].pointBackgroundColor = dataset.pointBackgroundColor;
+							dataset.pointBackgroundColor = helpers.color(color).alpha(fillAlpha).rgbString();
 						}
-						if (typeof dataset.pointBorderColor === 'undefined') {
+						if (typeof dataset.pointBorderColor === 'undefined' || override) {
+							dataset[EXPANDO_KEY].pointBorderColor = dataset.pointBorderColor;
 							dataset.pointBorderColor = color;
-							dataset.colorschemes.pointBorderColor = true;
 						}
 						break;
 					// For doughnut and pie chart, backgroundColor is set to an array of colors
 					case 'doughnut':
 					case 'pie':
-						if (typeof dataset.backgroundColor === 'undefined') {
+						if (typeof dataset.backgroundColor === 'undefined' || override) {
+							dataset[EXPANDO_KEY].backgroundColor = dataset.backgroundColor;
 							dataset.backgroundColor = dataset.data.map(function(data, dataIndex) {
 								colorIndex = dataIndex % length;
-								return schemeClone[options.reverse ? length - colorIndex - 1 : colorIndex];
+								return schemeClone[reverse ? length - colorIndex - 1 : colorIndex];
 							});
-							dataset.colorschemes.backgroundColor = true;
 						}
 						break;
 					// For the other chart, only backgroundColor is set
 					default:
-						if (typeof dataset.backgroundColor === 'undefined') {
+						if (typeof dataset.backgroundColor === 'undefined' || override) {
+							dataset[EXPANDO_KEY].backgroundColor = dataset.backgroundColor;
 							dataset.backgroundColor = color;
-							dataset.colorschemes.backgroundColor = true;
 						}
 						break;
 					}
@@ -98,20 +104,20 @@ export default {
 	afterUpdate: function(chart) {
 		// Unset colors
 		chart.config.data.datasets.forEach(function(dataset) {
-			if (dataset.colorschemes) {
-				if (dataset.colorschemes.backgroundColor) {
-					delete dataset.backgroundColor;
+			if (dataset[EXPANDO_KEY]) {
+				if (dataset[EXPANDO_KEY].hasOwnProperty('backgroundColor')) {
+					dataset.backgroundColor = dataset[EXPANDO_KEY].backgroundColor;
 				}
-				if (dataset.colorschemes.borderColor) {
-					delete dataset.borderColor;
+				if (dataset[EXPANDO_KEY].hasOwnProperty('borderColor')) {
+					dataset.borderColor = dataset[EXPANDO_KEY].borderColor;
 				}
-				if (dataset.colorschemes.pointBackgroundColor) {
-					delete dataset.pointBackgroundColor;
+				if (dataset[EXPANDO_KEY].hasOwnProperty('pointBackgroundColor')) {
+					dataset.pointBackgroundColor = dataset[EXPANDO_KEY].pointBackgroundColor;
 				}
-				if (dataset.colorschemes.pointBorderColor) {
-					delete dataset.pointBorderColor;
+				if (dataset[EXPANDO_KEY].hasOwnProperty('pointBorderColor')) {
+					dataset.pointBorderColor = dataset[EXPANDO_KEY].pointBorderColor;
 				}
-				delete dataset.colorschemes;
+				delete dataset[EXPANDO_KEY];
 			}
 		});
 	},
